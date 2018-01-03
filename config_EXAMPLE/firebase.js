24ddab6904c6a58f53c6976bd712a0318feefc55
Andrey Zimin (luummii) <luummii@vuejs.fun>
2018-01-03 14:07:52 +0300

    Add example config

---
+++ config_EXAMPLE/firebase.js
@@ -0,0 +1,9 @@
+// This data you can receive in your console firebase. path: Project setting --> Add firebase in your web application (button)
+export default {
+  apiKey: "UUUPSS",
+  authDomain: "UUUPSS",
+  databaseURL: "UUUPSS",
+  projectId: "UUUPSS",
+  storageBucket: "UUUPSS",
+  messagingSenderId: "UUUPSS"
+}
